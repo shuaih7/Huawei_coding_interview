3735590a474cd4ba11b007901e7e6f6efc044dbe
Shuaih7 <shuaih7@gmail.com>
2020-08-23 22:37:02 +0800

    Update HJ37.py

---
+++ HJ37.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def getTotalCount(n):
+    new1 =1
+    new2 =0
+    adult=0
+    for i in range(1,n):
+        adult+=new2
+        new2=new1
+        new1=adult
+    return new1+new2+adult
+
+if __name__ == "__main__":
+   while True:
+        try:
+            num=int(input())
+            print(getTotalCount(num))
+        except:
+            break
